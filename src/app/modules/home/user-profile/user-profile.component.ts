253841b5009834c66f3c5acc9482f6282c131b59
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2023-02-28 18:30:12 +0545

    [ADDED]: added profile page

--- src/app/modules/home/user-profile/user-profile.component.ts
+++ src/app/modules/home/user-profile/user-profile.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { LayoutComponent } from '../shared/layout/layout.component';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
-  providers: [{provide: 'Layout', useClass: LayoutComponent}]
+  // providers: [{provide: 'Layout', useClass: LayoutComponent}]
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  posts: any = [];
+  userProfilePic: any = null;
+  userName: any = null;
+  defaultImage = '/assets/images/default_image.png';
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.getPosts();
+
+  }
+
+  getPosts(): void{
+    this.postService.getPosts()
+    .subscribe(
+        result => {
+          this.userProfilePic = result.data.profile_img;
+          this.userName = result.data.name;
+          this.posts = result.uploads;
+          console.log(this.userProfilePic,this.posts);
+        },
+        error => {
+          console.log(error);
+        }
+    )
   }
 
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -3,8 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
-const baseUrl = "http://127.0.0.1:8000/api/posts";
-const createPostUrl = "http://127.0.0.1:8000/api/createPost";
+const url = "http://127.0.0.1:8000/api";
+const baseUrl = `${url}/posts`;
+const createPostUrl = `${url}/createPost`;
+const userProfileUrl = `${url}/me`;
 
 @Injectable({
   providedIn: 'root'
@@ -19,28 +21,18 @@ export class PostService {
 
   createPost(formData:any) {
     const auth_token = this.authService.getToken();
-    // let formData = new FormData();
-    // formData.append('post_img',image);
-    // formData.append('post_desc',desc!);
-    // let formDataJson = {
-    //   post_img: formData.get('post_img'),
-    //   post_desc: formData.get('post_desc'),
-    // }
-
-    // let formDataJson = JSON.stringify(formData);
-    // console.log(formDataJson);
+    
     const data:any = {};
     formData.forEach((val:any,key:any) => {
       data[key] = val;
     });
 
     console.log(data);
-
-    // const headers = new HttpHeaders({
-    //   'Authorization': `Bearer ${auth_token}`
-    // })
-    // return this.httpClient.post(createPostUrl,formData,{headers: headers});
     return this.httpClient.post(createPostUrl,formData);
+  }
+
+  getPosts(): Observable<any>{
+    return this.httpClient.get(userProfileUrl)
   }
 
 }
