3816f766bb9f08bad1fe17ffe2c5e48024036b71
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2023-03-08 16:59:14 +0545

    [REMOVED & ADDED]:removed hidden sidebar in homepage and added cache in user profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { Router } from '@angular/router';
 
 
@@ -10,40 +10,55 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private router:Router) { }
+  private authCache$?: Observable<any>;
+  constructor(private http: HttpClient, private router: Router) { }
 
-  login(params:any):Observable<any>{
-    
-    return this.http.post('http://127.0.0.1:8000/api/login',params);
+  login(params: any): Observable<any> {
+
+    return this.http.post('http://127.0.0.1:8000/api/login', params);
 
   }
 
-  isLoggedIn(){
+  isLoggedIn() {
     const token = localStorage.getItem('token');
-    if(token){
+    if (token) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  getToken(){
+  getToken() {
     const token = localStorage.getItem('token');
     return token;
   }
 
 
-  logout(){
+  logout() {
     let removeToken = localStorage.removeItem('token');
 
-    if(removeToken == null){
+    if (removeToken == null) {
       this.router.navigate(['/login']);
     }
   }
 
-  getPostsAuth(): Observable<any>{
+  getPostsAuth(): Observable<any> {
+    if (!this.authCache$) {
+      const authUserProfileUrl = 'http://127.0.0.1:8000/api/me';
+      this.authCache$ = this.http.get(authUserProfileUrl).pipe(
+        shareReplay(1)
+      )
+    }
+
+    return this.authCache$;
+  }
+
+  updateGetPostsAuth(): Observable<any> {
     const authUserProfileUrl = 'http://127.0.0.1:8000/api/me';
-    return this.http.get(authUserProfileUrl)
+    this.authCache$ = this.http.get(authUserProfileUrl).pipe(
+      shareReplay(1)
+    )
+    return this.authCache$;
   }
 
 }
